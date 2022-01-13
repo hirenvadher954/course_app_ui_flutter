eb9e44fb77425403ca9bef72ab3b0d61639143c6
hiren <hirenvadher954@gmail.com>
2022-01-13 21:55:04 +0530

    course_screen.dart completed

--- lib/screens/course_screen.dart
+++ lib/screens/course_screen.dart
@@ -1,5 +1,6 @@
 import 'package:facts_flutter_app/models/course.dart';
 import 'package:flutter/material.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 import '../constants.dart';
 
@@ -12,6 +13,33 @@ class CourseScreen extends StatefulWidget {
 }
 
 class _CourseScreenState extends State<CourseScreen> {
+  Widget indicators() {
+    List<Widget> indicators = [];
+
+    for (var i = 0; i < 9; i++) {
+      indicators.add(Container(
+        width: 7.0,
+        height: 7.0,
+        margin: EdgeInsets.symmetric(horizontal: 6.0),
+        decoration: BoxDecoration(
+            shape: BoxShape.circle,
+            color: i == 0 ? Color(0xFF0971FE) : Color(0xFFA6AEBD)),
+      ));
+    }
+
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: indicators,
+    );
+  }
+  late PanelController panelController;
+@override
+  void initState() {
+    super.initState();
+    panelController = PanelController();
+
+}
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -122,9 +150,195 @@ class _CourseScreenState extends State<CourseScreen> {
                         ),
                       ),
                     ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(right: 28.0),
+                    child: Container(
+                      padding: EdgeInsets.only(
+                        top: 12.5,
+                        bottom: 13.5,
+                        left: 20.5,
+                        right: 14.5,
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(16.0),
+                        boxShadow: [
+                          BoxShadow(
+                            color: kShadowColor,
+                            blurRadius: 16.0,
+                            offset: Offset(0, 4),
+                          ),
+                        ],
+                      ),
+                      width: 60.0,
+                      height: 60.0,
+                      child: Image.asset('asset/icons/icon-play.png'),
+                    ),
                   )
                 ],
-              )
+              ),
+              Padding(
+                padding: EdgeInsets.only(
+                  top: 12.0,
+                  left: 28.0,
+                  right: 28.0,
+                ),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Row(
+                      children: [
+                        Container(
+                          child: Padding(
+                            padding: EdgeInsets.all(4.0),
+                            child: Container(
+                              padding: EdgeInsets.all(4.0),
+                              child: CircleAvatar(
+                                child: Icon(
+                                  Icons.people,
+                                  color: Colors.white,
+                                ),
+                                radius: 21.0,
+                                backgroundColor: kCourseElementIconColor,
+                              ),
+                              decoration: BoxDecoration(
+                                color: kBackgroundColor,
+                                borderRadius: BorderRadius.circular(29.0),
+                              ),
+                            ),
+                          ),
+                          height: 58.0,
+                          width: 58.0,
+                          decoration: BoxDecoration(
+                            gradient: widget.course.background,
+                            borderRadius: BorderRadius.circular(29.0),
+                          ),
+                        ),
+                        SizedBox(width: 12.0),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "28.7k",
+                              style: kTitle2Style,
+                            ),
+                            Text(
+                              "Students",
+                              style: kSearchPlaceholderStyle,
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                    Row(
+                      children: [
+                        Container(
+                          child: Padding(
+                            padding: EdgeInsets.all(4.0),
+                            child: Container(
+                              padding: EdgeInsets.all(4.0),
+                              child: CircleAvatar(
+                                child: Icon(
+                                  Icons.format_quote,
+                                  color: Colors.white,
+                                ),
+                                radius: 21.0,
+                                backgroundColor: kCourseElementIconColor,
+                              ),
+                              decoration: BoxDecoration(
+                                color: kBackgroundColor,
+                                borderRadius: BorderRadius.circular(29.0),
+                              ),
+                            ),
+                          ),
+                          height: 58.0,
+                          width: 58.0,
+                          decoration: BoxDecoration(
+                            gradient: widget.course.background,
+                            borderRadius: BorderRadius.circular(29.0),
+                          ),
+                        ),
+                        SizedBox(width: 12.0),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "12.4k",
+                              style: kTitle2Style,
+                            ),
+                            Text(
+                              "Comments",
+                              style: kSearchPlaceholderStyle,
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 28.0, vertical: 24.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    indicators(),
+                    GestureDetector(
+                      onTap: () {
+                        panelController.open();
+                      },
+                      child: Container(
+                        alignment: Alignment.center,
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          boxShadow: [
+                            BoxShadow(
+                              color: kShadowColor,
+                              offset: Offset(0, 12),
+                              blurRadius: 16.0,
+                            )
+                          ],
+                          borderRadius: BorderRadius.circular(14.0),
+                        ),
+                        width: 80.0,
+                        height: 40.0,
+                        child: Text(
+                          "View all",
+                          style: kSearchTextStyle,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 28.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "5 years ago, I couldn’t write a single line of Swift. I learned it and moved to React, Flutter while using increasingly complex design tools. I don’t regret learning them because SwiftUI takes all of their best concepts. It is hands-down the best way for designers to take a first step into code.",
+                      style: kBodyLabelStyle,
+                    ),
+                    SizedBox(
+                      height: 24.0,
+                    ),
+                    Text(
+                      "About this course",
+                      style: kTitle1Style,
+                    ),
+                    SizedBox(
+                      height: 24.0,
+                    ),
+                    Text(
+                      "This course was written for people who are passionate about design and about Apple's SwiftUI. It beginner-friendly, but it is also packed with tricks and cool workflows about building the best UI. Currently, Xcode 11 is still in beta so the installation process may be a little hard. However, once you get everything working, then it'll get much friendlier!",
+                      style: kBodyLabelStyle,
+                    ),
+                    SizedBox(height: 24.0),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
